1451d8e82b125703db6ae038ce4b5afc5efa9873
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-09-09 23:01:26 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,7 @@
+public class Day2{
+  public static void main(String[] args){
+    System.out.print("informatika");
+    System.out.println("yudha");
+    System.out.printf("nama : %s, umur : %d %s %n", "tirta prayudha", 18, " tahun");
+  }
+}
