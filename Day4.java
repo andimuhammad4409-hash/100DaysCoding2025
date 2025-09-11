09a42b6cf8b7e214a22a494390642f644e84321d
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-09-11 21:59:40 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,8 @@
+public class Day4{
+  public static void main(String[] args){
+    System.out.println("1. meja \n2. kursi");
+    System.out.println("nama \t saya \t yudha");
+    System.out.println("belajar \"java\"");
+    System.out.println("path: C\\Users\\user");
+  }
+}
