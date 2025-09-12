50bfa4058ca078708017bff5a94c336ab5bbece3
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-09-12 19:46:24 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,13 @@
+public class Day5 {
+    public static void main(String[] args){
+        int angka1 = 12;
+        short angka2 = 1234;
+        byte angka3 = 126;
+        long angka4 = 1000000000L;
+        
+        System.out.println(angka1);
+        System.out.println(angka2);
+        System.out.println(angka3);
+        System.out.println(angka4);
+    }
+}
