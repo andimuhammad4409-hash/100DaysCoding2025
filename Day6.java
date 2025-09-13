2f87ac0c2de41ad05afee34cbfd07988efc8f7cd
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-09-13 20:39:56 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,9 @@
+public class Day6{
+  public static void main(String[] args){
+    double kecepatan = 2.4;
+    float bensin = 1;
+
+    System.out.println("kecepatan anda : "+ kecepatan + " km");
+    System.out.println("bensin anda : "+ bensin + " liter");
+  }
+}
