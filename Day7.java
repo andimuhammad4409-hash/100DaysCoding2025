1af329f482683881efeefa5db6a86530f44c0c69
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-09-14 16:33:26 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,9 @@
+public class Day7{
+  public static void main(String[] args){
+    char kelas = 'c';
+    boolean kebenaran = true;
+
+    System.out.println("saya kelas : "+ kelas);
+    System.out.println("apakah saya kelas c : "+ kebenaran);
+  }
+}
