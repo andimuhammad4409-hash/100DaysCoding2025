e78f2ec12152754e4edde3e2a77c9f7baec4de7a
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-09-15 20:24:31 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,9 @@
+public class Day8{
+  public static void main(String[] args){
+    String nama = "yudha";
+    String alamat = "desa mambu";
+
+    System.out.println("nama saya adalah "+ nama);
+    System.out.println("saya berasal dari "+ alamat);
+  }
+}
