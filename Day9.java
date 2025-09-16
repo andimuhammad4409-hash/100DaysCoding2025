219cf07bc5417c88f480c344a662aeabe90839a9
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-09-16 20:06:36 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,9 @@
+public class Day9{
+  public static void main(String[] args){
+    int nomor = 5;
+    System.out.println("nilai awal = "+ nomor);
+
+    nomor = 7;
+    System.out.println("nilai yang di update = "+ nomor);
+  }
+}
