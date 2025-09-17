bbe030ce2cab23f1a22368352a5965eead05a966
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-09-17 21:38:18 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,24 @@
+public class Day10{
+  public static void main(String[] args){
+
+    final String nama = "yudha";
+    final int tahun = 2007;
+    final byte bulan = 11;
+    final short tanggal = 20;
+    final float kecepatan = 2.5f;
+    final double uang = 20.000;
+    final char kelas = 'C';
+    final boolean jawaban = true;
+    final long angka = 12345;
+
+    System.out.println("nama saya "+ nama);
+    System.out.println("sekarang tahun "+ tahun);
+    System.out.println("sekarang tanggal "+ tanggal);
+    System.out.println("sekarang bulan "+ bulan);
+    System.out.println("jaraknya adalah "+ kecepatan);
+    System.out.println("sisa uang Rp. "+ uang);
+    System.out.println("saya dari kelas "+ kelas);
+    System.out.println("jawaban anda "+ jawaban);
+    System.out.println("angka yang berurut : "+ angka);
+  }
+}
