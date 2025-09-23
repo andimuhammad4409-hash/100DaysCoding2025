722fabfe4d1e8d580dd7184189d213fa029e4cbc
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-09-23 20:04:17 +0800

    Add files via upload

---
+++ Day16.java
@@ -0,0 +1,10 @@
+
+public class Day16 {
+    public static void main(String[] args){
+        int angka1 = 4;
+        int angka2 = 2;
+        
+        System.out.printf("%d x %d = %d \n", angka1,angka2,angka1*angka2);
+        System.out.printf("%d : %d = %d", angka1,angka2,angka1/angka2);
+    }
+}
