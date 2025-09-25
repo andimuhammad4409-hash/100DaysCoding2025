9bdd97554c61a3f2a72fbe7540bde2227969949c
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-09-25 21:53:57 +0800

    Add files via upload

---
+++ Day18.java
@@ -0,0 +1,10 @@
+
+public class Day18 {
+    public static void main(String[] args){
+        byte angka = 120;
+        
+        short angkalebihbesar = angka;
+        
+        System.out.println(angka);
+    }
+}
