e1849c3752207335280ddca396e34517e4d48262
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-09-27 17:39:01 +0800

    Add files via upload

---
+++ Day20.java
@@ -0,0 +1,12 @@
+
+public class Day20 {
+    public static void main(String[] args){
+        int angka = 12;
+        
+        String angka_string = String.valueOf(angka);
+        
+        System.out.println(angka_string);
+        
+        System.out.println(angka_string + angka);
+    }
+}
