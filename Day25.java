f2a446c9f07ff140a8ccfc4bf4a577d185d8e32f
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-10-02 23:06:29 +0800

    Add files via upload

---
+++ Day25.java
@@ -0,0 +1,47 @@
+
+public class Day25 {
+    public static void main(String[] args){
+        
+     //increment
+      int a = 4;  
+      a++;  
+      System.out.println(a);  
+        
+     //prefix   
+     int x = 5;
+     int y = ++x;
+        
+     System.out.println(x);   
+     System.out.println(y);
+        
+     //postfix
+        
+      int x1 = 5;
+      int y1 = x1++;
+        
+    System.out.println(x1);    
+    System.out.println(y1);
+        
+     System.out.println("##################");
+    //decrement
+      int b = 4;  
+      b--;  
+      System.out.println(b);  
+        
+     //prefix   
+     int c = 5;
+     int d = --c;
+        
+     System.out.println(c);   
+     System.out.println(d);
+        
+     //postfix
+        
+      int c1 = 5;
+      int d1 = c1--;
+        
+    System.out.println(c1);    
+    System.out.println(d1);        
+        
+    }
+}
