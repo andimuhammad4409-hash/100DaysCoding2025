ee2da47eea63d6b231a59afa967132a6486eba9f
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-10-05 22:00:16 +0800

    Add files via upload

---
+++ Day28.java
@@ -0,0 +1,10 @@
+
+public class Day28 {
+   public static void main(String[] args) {
+      int a = 1;
+      int b = 2;
+       
+       System.out.println(a == b);
+       System.out.println(a != b);
+   }
+}
