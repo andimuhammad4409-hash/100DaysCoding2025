a11579534e08f17635b973c76ee9f486e788f33b
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-10-09 22:14:48 +0800

    Add files via upload

---
+++ Day32.java
@@ -0,0 +1,19 @@
+
+public class Day32 {
+    public static void main(String[] args){
+        boolean a = true;
+        boolean b = true;
+        
+        System.out.println(a || b);
+        
+        a = true;
+        b = false;
+        
+        System.out.println(a || b);
+        
+        a = false;
+        b = false;
+        
+        System.out.println(a || b);
+    }
+}
