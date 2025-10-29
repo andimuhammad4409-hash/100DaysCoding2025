c61938892419df5adc76b6d3f0407938467ec06a
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-10-29 23:22:57 +0800

    Add files via upload

---
+++ Day52.java
@@ -0,0 +1,14 @@
+
+public class Day52 {
+    public static void main(String[] args){
+       int hari = 1;
+        while (hari <= 7) {
+            
+           System.out.println("hari ke-"+hari);
+            
+            hari++;
+        }
+        
+        
+    }
+}
