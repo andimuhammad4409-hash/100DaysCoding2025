67a20b6631e50827998bb4050c1ff062b9c6f0d1
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-11-06 19:50:07 +0800

    Add files via upload

---
+++ Day60.java
@@ -0,0 +1,22 @@
+
+public class Day60 {
+    public static void main(String[] args){
+        for (int i = 10;i >= 1;i-- ) {
+            
+            if (i % 2 == 0) {
+                
+                System.out.println(i);
+            }
+        }   
+        System.out.println("------------------");    
+        for (int i = 10;i >= 1;i-- ) {
+            
+            if (i % 2 == 1) {
+                
+                System.out.println(i);
+            }    
+            
+            
+        }
+    }
+}
