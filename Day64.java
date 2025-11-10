d7749e5a9f0fa490b599341516397228847fb4bd
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-11-10 18:28:03 +0800

    Add files via upload

---
+++ Day64.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Day64 {
+    public static void main(String[] args) {
+        int total = 1;
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("masukan angka m : ");
+        int m = input.nextInt();
+        
+        System.out.print("masukan pangkat : ");
+        int pangkat = input.nextInt();
+        
+        
+        if (pangkat == 0) {
+            total = 1;
+        } else {
+            for (int i = 1;i <= pangkat ;i++) {
+            System.out.print(m);
+            
+            total *= m;
+        }
+        }
+        
+        System.out.println(total);
+        
+        
+        
+        
+    }
+}
