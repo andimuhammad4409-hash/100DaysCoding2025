8c60843b7e5378358c43afd791b5519413d3941a
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-11-17 23:27:23 +0800

    Add files via upload

---
+++ Day71.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Day71 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("masukan tinggi : ");
+        int n = input.nextInt();
+        
+        for (int i = 1;i <= n;i++) {
+            for (int j = 1;j <= i;j++) {
+                System.out.print("* ");
+            }
+            
+            System.out.println();
+        }
+    }
+}
