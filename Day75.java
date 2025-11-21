55a4c751cf8fdd4b02354146757b4f20077bcb80
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-11-21 19:10:28 +0800

    Add files via upload

---
+++ Day75.java
@@ -0,0 +1,14 @@
+
+public class Day75 {
+    public static void main(String[] args) {
+        String nama = "fulan";
+        
+        int panjang = nama.length();
+        String kecil = nama.toLowerCase();
+        String besar = nama.toUpperCase();
+        
+        System.out.println(panjang);
+        System.out.println(kecil);
+        System.out.println(besar);
+    }
+}
