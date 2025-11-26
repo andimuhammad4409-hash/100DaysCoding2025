5478ee2b2eb405d8bde872e1e47430888233919b
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-11-26 21:21:43 +0800

    Add files via upload

---
+++ Day80.java
@@ -0,0 +1,20 @@
+
+public class Day80 {
+    public static void main(String[] args) {
+        
+        //inisialisasi
+        int[] angka = {1,2,3,4,5};
+        
+        System.out.println(angka[4]);
+        
+        //deklarasi
+        
+        int[] angka1 = new int[3];
+        
+        angka1[0] = 6;
+        angka1[1] = 7;
+        angka1[2] = 8;
+        
+        System.out.println(angka1[2]);
+    }
+}
