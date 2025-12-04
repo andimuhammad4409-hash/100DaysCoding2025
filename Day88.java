406be3320cfd896917220d8063d48de6a8709f34
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-12-04 23:37:14 +0800

    Add files via upload

---
+++ Day88.java
@@ -0,0 +1,9 @@
+public class Day88 {
+    public static void main(String[] args) {
+        char[] karakter = {'S','A','Y','A'};
+        
+        for (char c : karakter) {
+            System.out.println(c);
+        }
+    }
+}
