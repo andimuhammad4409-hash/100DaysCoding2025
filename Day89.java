d4d13af65afae96a2af67ff076d4ac26970af2f6
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-12-05 23:30:10 +0800

    Add files via upload

---
+++ Day89.java
@@ -0,0 +1,15 @@
+public class Day89 {
+   public static void main(String[] args) {
+       garis();
+       biodata("yudha", 18);
+   }
+    
+    static void garis() {
+        System.out.println("-----------------------");
+    }
+    
+    static void biodata(String nama, int umur) {
+        System.out.println("nama saya "+ nama);
+        System.out.println("umur saya "+ umur+ " tahun");
+    }
+}
