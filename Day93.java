14892903819d3c05897af70f8c11ab080c2f3b79
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-12-09 22:18:48 +0800

    Add files via upload

---
+++ Day93.java
@@ -0,0 +1,9 @@
+public class Day93 {
+    public static void main(String[] args) {
+        double x = 10.5;
+        
+        System.out.println("ceil  : "+ Math.ceil(x));
+        System.out.println("floor : "+ Math.floor(x));
+        System.out.println("round : "+ Math.round(x));
+    }
+}
