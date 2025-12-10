6cf626daf1a0238873e90b0290aea21b61a7e761
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-12-10 22:06:05 +0800

    Add files via upload

---
+++ Day94.java
@@ -0,0 +1,11 @@
+public class Day94 {
+   public static void main(String[] args) {
+     salam();
+     salam();
+     salam();  
+   }
+    
+   static void salam() {
+       System.out.println("hai");
+   }
+}
